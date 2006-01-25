9d7329774ef1e9870a26cbe2c03106a1c4d12e9b
creich <creich@9c94448d-38f1-0310-a231-d98308ff1ebf>
2006-01-25 20:15:47 +0000

    git-svn-id: svn://svn.caucho.com/resin/trunk@613 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/lib/QuercusZlibModule.java
+++ modules/quercus/src/com/caucho/quercus/lib/QuercusZlibModule.java
@@ -30,16 +30,20 @@ package com.caucho.quercus.lib;
 
 import com.caucho.quercus.env.BooleanValue;
 import com.caucho.quercus.env.Env;
+import com.caucho.quercus.env.StringValue;
 import com.caucho.quercus.env.Value;
 import com.caucho.quercus.module.AbstractQuercusModule;
 import com.caucho.quercus.module.NotNull;
 import com.caucho.quercus.module.Optional;
+import com.caucho.util.ByteBuffer;
 import com.caucho.util.L10N;
 import com.caucho.util.Log;
 
 import java.io.IOException;
 import java.util.logging.Logger;
 import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 /**
  * PHP ZLib
@@ -48,6 +52,9 @@ public class QuercusZlibModule extends AbstractQuercusModule {
 
   private static final Logger log = Log.open(QuercusZlibModule.class);
   private static final L10N L = new L10N(QuercusZlibModule.class);
+
+  public static final int FORCE_GZIP = 0x1;
+  public static final int FORCE_DEFLATE = 0x2;
 
   /**
    * Returns true for the Zlib extension.
@@ -117,10 +124,10 @@ public class QuercusZlibModule extends AbstractQuercusModule {
   {
     if (zp == null)
       return false;
-    
+
     return zp.gzeof();
   }
-  
+
   public Value gzgetc(@NotNull ZlibClass zp)
     throws IOException, DataFormatException
   {
@@ -139,7 +146,7 @@ public class QuercusZlibModule extends AbstractQuercusModule {
 
     return zp.gzgets(length);
   }
-  
+
   public Value gzgetss(@NotNull ZlibClass zp,
                        int length,
                        @Optional String allowedTags)
@@ -147,21 +154,147 @@ public class QuercusZlibModule extends AbstractQuercusModule {
   {
     if (zp == null)
       return BooleanValue.FALSE;
-    
+
     return zp.gzgetss(length,allowedTags);
   }
 
-  // @todo gzcompress()
-  // @todo gzdeflate()
+  /**
+   * compresses data using zlib
+   * 
+   * @param data
+   * @param level (default is Deflater.DEFAULT_COMPRESSION)
+   * @return compressed string
+   */
+  public Value gzcompress(String data,
+                          @Optional("-1") int level)
+    throws DataFormatException
+  {
+    if (level == -1)
+      level = Deflater.DEFAULT_COMPRESSION;
+
+    Deflater deflater = new Deflater(level);
+    byte[] input = data.getBytes();
+    deflater.setInput(input);
+    deflater.finish();
+
+    byte[] output = new byte[2 * input.length];
+    int compressedLength = deflater.deflate(output);
+
+    return new StringValue(new String(output,0,compressedLength));
+  }
+
+  /**
+   * 
+   * @param data
+   * @param length (maximum length of string returned)
+   * @return uncompressed string
+   */
+  public Value gzuncompress(String data,
+                            @Optional("0") int length)
+    throws DataFormatException
+  {
+    byte[] input = data.getBytes();
+    byte[] output = new byte[input.length];
+    Inflater inflater = new Inflater();
+    inflater.setInput(input,0,input.length);
+    int uncompressedLength = 0;
+    int fullUncompressedLength = 0;
+    ByteBuffer buf = new ByteBuffer();
+    
+    while(!inflater.finished()) {
+      uncompressedLength = inflater.inflate(output);
+      fullUncompressedLength += uncompressedLength;
+      buf.append(output,0,uncompressedLength);
+    }
+    
+    if (length == 0)
+      length = fullUncompressedLength;
+    else
+      length = Math.min(fullUncompressedLength, length);
+    
+    return new StringValue(new String(output,0,length));
+  }
+
+  /**
+   * 
+   * @param data
+   * @param level
+   * @return compressed using DEFLATE algorithm
+   */
+  public Value gzdeflate(String data,
+                         @Optional("-1") int level)
+   throws DataFormatException
+  {
+    if (level == -1)
+      level = Deflater.DEFAULT_COMPRESSION;
+
+    Deflater deflater = new Deflater(level, true);
+    byte[] input = data.getBytes();
+    deflater.setInput(input);
+    deflater.finish();
+
+    byte[] output = new byte[2 * input.length];
+    int compressedLength = deflater.deflate(output);
+    /*
+    // The following will be taken out.  Only to test inflate
+    byte[] result = new byte[output.length];
+    int uncompressedLength = 0;
+    int fullUncompressedLength = 0;
+    ByteBuffer buf = new ByteBuffer();
+    Inflater inflater = new Inflater(true);
+    inflater.setInput(output,0,output.length);
+    
+    while (!inflater.finished()) {
+      uncompressedLength = inflater.inflate(result);
+      fullUncompressedLength += uncompressedLength;
+      buf.append(result,0,uncompressedLength);
+    }
+    
+    return new StringValue(new String(buf.getBuffer()));
+    // END OF CODE TO TAKE OUT LATER
+    */
+    return new StringValue(new String(output,0,compressedLength));
+  }
+
+  /**
+   * 
+   * @param data compressed using Deflate algorithm
+   * @param length (maximum length of string returned)
+   * @return uncompressed string
+   */
+  public Value gzinflate(String data,
+                         @Optional("0") int length)
+    throws DataFormatException
+  {
+    byte[] input = data.getBytes();
+    byte[] output = new byte[input.length];
+    Inflater inflater = new Inflater(true);
+    inflater.setInput(input,0,input.length);
+    int uncompressedLength = 0;
+    int fullUncompressedLength = 0;
+    ByteBuffer buf = new ByteBuffer();
+    
+    while(!inflater.finished()) {
+      uncompressedLength = inflater.inflate(output);
+      fullUncompressedLength += uncompressedLength;
+      buf.append(output,0,uncompressedLength);
+    }
+    
+    if (length == 0)
+      length = fullUncompressedLength;
+    else
+      length = Math.min(fullUncompressedLength, length);
+    
+    return new StringValue(new String(output,0,length));
+  }
+  
   // @todo gzencode()
   // @todo gzfile() -- XXX Skip for now
-  // @todo gzinflate()
   // @todo gzpassthru() -- XXX Skip for now
   // @todo gzread()
   // @todo gzrewind()
   // @todo gzseek() -- XXX Skip for now
   // @todo gztell() -- XXX Skip for now
-  // @todo gzuncompress()
   // @todo readgzfile() -- XXX Skip for now
   // @todo zlib_get_coding_type()
 }
