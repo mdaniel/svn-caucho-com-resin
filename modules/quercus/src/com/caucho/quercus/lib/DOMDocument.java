24509bbc97077640826c1596017b32d55ab55c73
creich <creich@9c94448d-38f1-0310-a231-d98308ff1ebf>
2006-02-15 21:28:08 +0000

    more DOM
    git-svn-id: svn://svn.caucho.com/resin/trunk@736 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/lib/DOMDocument.java
+++ modules/quercus/src/com/caucho/quercus/lib/DOMDocument.java
@@ -1,32 +1,50 @@
 package com.caucho.quercus.lib;
 
+import org.w3c.dom.DOMConfiguration;
+
 /**
  * Created by IntelliJ IDEA. User: Charles Reich Date: Feb 15, 2006 Time:
  * 2:56:27 PM To change this template use File | Settings | File Templates.
  */
 public class DOMDocument extends DOMNode {
-  //PROPERTIES
-  //@todo actualEncoding (String)
-  //@todo config (DOMConfiguration)
-  //@todo doctype (DOMDocumentType)
-  //@todo documentElement (DOMElement)
-  //@todo documentURI (String)
-  //@todo encoding (String)
-  //@todo formatOutput (boolean)
-  //@todo implementation (DOMImplementation)
-  //@todo preserveWhiteSpace (boolean)
-  //@todo recover (boolean)
-  //@todo resolveExternals (boolean)
-  //@todo standalone (boolean)
-  //@todo strictErrorChecking (boolean)
-  //@todo substituteEntities (boolean)
-  //@todo validateOnParse (boolean)
-  //@todo version (String)
-  //@todo xmlEncoding (String)
-  //@todo xmlStandalone (boolean)
-  //@todo xmlVersion (String)
+
+  public String actualEncoding;
+  public DOMConfiguration config;
+  public DOMDocumentType doctype;
+  public DOMElement documentElement;
+  public String documentURI;
+  public String encoding;
+  public boolean formatOutput;
+  public DOMImplementation implementation;
+  public boolean preserveWhiteSpace;
+  public boolean recover;
+  public boolean resolveExternals;
+  public boolean standalone;
+  public boolean strictErrorChecking;
+  public boolean substituteEntities;
+  public boolean validateOnParse;
+  public String version;
+  public String xmlEncoding;
+  public boolean xmlStandalone;
+  public String xmlVersion;
+
+  public DOMDocument()
+  {
+  }
   
+  public DOMDocument(String version)
+  {
+    this.version = version;
+  }
   
+  public DOMDocument(String version,
+                     String encoding)
+  {
+    this.version = version;
+    this.encoding = encoding;
+  }
+  
+
   //METHODS
   //@todo construct()
   //@todo createAttribute()
