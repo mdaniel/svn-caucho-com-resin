9d5fab0ba1bf6b31f7e56998cd4d073a865ba1dc
nam <nam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2006-08-22 13:18:35 +0000

    json code review changes
    git-svn-id: svn://svn.caucho.com/resin/trunk@1462 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/quercus/src/com/caucho/quercus/lib/json/JsonDecoder.java
@@ -0,0 +1,460 @@
+/*
+ * Copyright (c) 1998-2006 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Nam Nguyen
+ */
+
+package com.caucho.quercus.lib.json;
+
+import com.caucho.quercus.env.ArrayValue;
+import com.caucho.quercus.env.ArrayValueImpl;
+import com.caucho.quercus.env.BooleanValue;
+import com.caucho.quercus.env.DoubleValue;
+import com.caucho.quercus.env.Env;
+import com.caucho.quercus.env.LongValue;
+import com.caucho.quercus.env.NullValue;
+import com.caucho.quercus.env.NumberValue;
+import com.caucho.quercus.env.ObjectValue;
+import com.caucho.quercus.env.StringBuilderValue;
+import com.caucho.quercus.env.StringValue;
+import com.caucho.quercus.env.Value;
+
+import com.caucho.util.L10N;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class JsonDecoder {
+  private static final L10N L = new L10N(JsonDecoder.class);
+
+  private StringValue _string;
+  private int _len;
+  private int _offset;
+
+  private boolean _isAssociative;
+
+  public Value jsonDecode(Env env,
+                          StringValue s,
+                          boolean assoc)
+  {
+    _string = s;
+    _len = _string.length();
+    _offset = 0;
+
+    _isAssociative = assoc;
+
+    Value val = jsonDecodeImpl(env);
+
+    // Should now be at end of string or have only white spaces left.
+    if (skipWhitespace() >= 0)
+      return errorReturn(env, "expected no more input");
+
+    return val;
+  }
+
+  /**
+   * Entry point to decode a JSON value.
+   *
+   * @return decoded PHP value 
+   */
+  private Value jsonDecodeImpl(Env env)
+  {
+    for (int ch = skipWhitespace(); ch >= 0; ch = read()) {
+
+      switch (ch) {
+        case '"':
+          return decodeString(env);
+
+        case 't':
+          if (read() == 'r' &&
+              read() == 'u' &&
+              read() == 'e')
+            return BooleanValue.TRUE;
+          else
+            return errorReturn(env, "expected 'true'");
+
+        case 'f':
+          if (read() == 'a' &&
+              read() == 'l' &&
+              read() == 's' &&
+              read() == 'e')
+            return BooleanValue.FALSE;
+          else
+            return errorReturn(env, "expected 'false'");
+
+        case 'n':
+          if (read() == 'u' &&
+              read() == 'l' &&
+              read() == 'l')
+            return NullValue.NULL;
+          else
+            return errorReturn(env, "expected 'null'");
+
+        case '[':
+          return decodeArray(env);
+
+        case '{':
+          return decodeObject(env);
+
+        default:
+          if (ch == '-' || ('0' <= ch && ch <= '9'))
+            return decodeNumber(env, ch);
+          else
+            return errorReturn(env);
+      }
+    }
+
+    return errorReturn(env);
+  }
+
+  /**
+   * Checks to see if there is a valid number per JSON Internet Draft.
+   */
+  private Value decodeNumber(Env env, int ch)
+  {
+    StringBuilder sb = new StringBuilder();
+
+    // (-)?
+    if (ch == '-') {
+      sb.append((char)ch);
+      ch = read();
+    }
+
+    // (0) | ([1-9] [0-9]+)
+    if (ch >= 0) {
+      if (ch == '0') {
+        sb.append((char)ch);
+        ch = read();
+      }
+      else if ('1' <= ch && ch <= '9') {
+        sb.append((char)ch);
+        ch = read();
+
+        while ('0' <= ch && ch <= '9') {
+          sb.append((char)ch);
+          ch = read();
+        }
+      }
+      else
+        return errorReturn(env, "expected 1-9");
+    }
+
+    int integerEnd = sb.length();
+
+    // ((decimalPoint) [0-9]+)?
+    if (ch == '.') {
+      sb.append((char)ch);
+      ch = read();
+
+      while ('0' <= ch && ch <= '9') {
+        sb.append((char)ch);
+        ch = read();
+      }
+    }
+
+    // ((e | E) (+ | -)? [0-9]+)
+    if (ch == 'e' || ch == 'E') {
+      sb.append((char)ch);
+      ch = read();
+
+      if (ch == '+' || ch == '-') {
+        sb.append((char)ch);
+        ch = read();
+      }
+
+      if ('0' <= ch && ch <= '9') {
+        sb.append((char)ch);
+        ch = read();
+
+        while ('0' <= ch && ch <= '9') {
+          sb.append((char)ch);
+          ch = read();
+        }
+      }
+      else
+        return errorReturn(env, "expected 0-9 exponent");
+    }
+
+    unread();
+
+    if (integerEnd != sb.length())
+      return new DoubleValue(Double.parseDouble(sb.toString()));
+    else
+      return new LongValue(Long.parseLong(sb.toString()));
+  }
+
+  /**
+   * Returns a non-associative PHP array.
+   */
+  private Value decodeArray(Env env)
+  {
+    ArrayValueImpl array = new ArrayValueImpl();
+
+    while (true) {
+      int ch = skipWhitespace();
+
+      if (ch == ']')
+        break;
+
+      unread();
+
+      array.append(jsonDecodeImpl(env));
+
+      ch = skipWhitespace();
+
+      if (ch == ',') {
+      }
+      else if (ch == ']')
+        break;
+      else
+        errorReturn(env, "expected either ',' or ']'");
+    }
+
+    return array;
+  }
+
+  private Value decodeObject(Env env)
+  {
+    if (_isAssociative)
+      return decodeObjectToArray(env);
+    else
+      return decodeObjectToObject(env);
+  }
+
+  /**
+   * Returns a PHP associative array of JSON object.
+   */
+  private Value decodeObjectToArray(Env env)
+  {
+    ArrayValue array = new ArrayValueImpl();
+
+    while (true) {
+      int ch = skipWhitespace();
+
+      if (ch == '}')
+        break;
+
+      unread();
+
+      Value name = jsonDecodeImpl(env);
+
+      ch = skipWhitespace();
+
+      if (ch != ':')
+        return errorReturn(env, "expected ':'");
+
+      array.append(name, jsonDecodeImpl(env));
+
+      ch = skipWhitespace();
+
+      if (ch == ',') {
+      }
+      else if (ch == '}')
+        break;
+      else
+        return errorReturn(env, "expected either ',' or '}'");
+    }
+
+    return array;
+  }
+
+  /**
+   * Returns a PHP stdObject of JSON object.
+   */
+  private Value decodeObjectToObject(Env env)
+  {
+    ObjectValue object = env.createObject();
+
+    while (true) {
+      int ch = skipWhitespace();
+
+      if (ch == '}')
+        break;
+
+      unread();
+
+      Value name = jsonDecodeImpl(env);
+
+      ch = skipWhitespace();
+
+      if (ch != ':')
+        return errorReturn(env, "expected ':'");
+
+      object.putFieldInit(env, name.toString(), jsonDecodeImpl(env));
+
+      ch = skipWhitespace();
+
+      if (ch == ',') {
+      }
+      else if (ch == '}')
+        break;
+      else
+        return errorReturn(env, "expected either ',' or '}'");
+    }
+
+    return object;
+  }
+
+  /**
+   * Returns a PHP string.
+   */
+  private Value decodeString(Env env)
+  {
+    StringBuilderValue sbv = new StringBuilderValue();
+
+    for (int ch = read(); ch >= 0; ch = read()) {
+
+      switch (ch) {
+        // Escaped Characters
+        case '\\':
+          ch = read();
+          if (ch < 0)
+            return errorReturn(env, "invalid escape character");
+
+          switch (ch) {
+            case '"':
+              sbv.append('"');
+              break;
+            case '\\':
+              sbv.append('\\');
+              break;
+            case '/':
+              sbv.append('/');
+              break;
+            case 'b':
+              sbv.append('\b');
+              break;
+            case 'f':
+              sbv.append('\f');
+              break;
+            case 'n':
+              sbv.append('\n');
+              break;
+            case 'r':
+              sbv.append('\r');
+              break;
+            case 't':
+              sbv.append('\t');
+              break;
+            case 'u':
+            case 'U':
+              int hex = 0;
+
+              for (int i = 0; i < 4; i++) {
+                hex = hex << 4;
+                ch = read();
+
+                if ('0' <= ch && ch <= '9')
+                  hex += ch - '0';
+                else if (ch >= 'a' && ch <= 'f')
+                  hex += ch - 'a' + 10;
+                else if (ch >= 'A' && ch <= 'F')
+                  hex += ch - 'A' + 10;
+                else
+                  return errorReturn(env, "invalid escaped hex character");
+              }
+
+              sbv.append((char)hex);
+
+          }
+          break;
+
+        case '"':
+          return sbv;
+
+        default:
+          sbv.append((char)ch);
+      }
+    }
+
+    return errorReturn(env, "error decoding string");
+  }
+
+  private Value errorReturn(Env env)
+  {
+    return errorReturn(env, null);
+  }
+
+  private Value errorReturn(Env env, String message)
+  {
+    int start;
+    int end;
+
+    if (_offset < _len) {
+      start = _offset - 1;
+      end = _offset;
+    }
+    else {
+      start = _len - 1;
+      end = _len;
+    }
+
+    String token = _string.substring(start, end).toString();
+
+    if (message != null)
+      env.warning(L.l("error parsing '{0}': {1}", token, message));
+    else
+      env.warning(L.l("error parsing '{0}'", token));
+
+    return NullValue.NULL;
+  }
+
+  private void unread()
+  {
+    if (_offset > 0)
+      _offset--;
+  }
+
+  private int peek(int index)
+  {
+    if (0 <= index && index < _len)
+      return _string.charAt(index);
+    else
+      return -1;
+  }
+
+  private int read()
+  {
+    if (_offset < _len)
+      return _string.charAt(_offset++);
+    else
+      return -1;
+  }
+
+  private int skipWhitespace()
+  {
+    int ch = read();
+    for (; ch >= 0; ch = read()) {
+      if (ch != ' ' &&
+          ch != '\n' &&
+          ch != '\r' &&
+          ch != '\t')
+        break;
+    }
+
+    return ch;
+  }
+}
---
+++ modules/quercus/src/com/caucho/quercus/lib/json/JsonModule.java
@@ -0,0 +1,268 @@
+/*
+ * Copyright (c) 1998-2006 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Nam Nguyen
+ */
+
+package com.caucho.quercus.lib.json;
+
+import com.caucho.quercus.env.ArrayValue;
+import com.caucho.quercus.env.ArrayValueImpl;
+import com.caucho.quercus.env.BooleanValue;
+import com.caucho.quercus.env.Env;
+import com.caucho.quercus.env.NullValue;
+import com.caucho.quercus.env.NumberValue;
+import com.caucho.quercus.env.ObjectValue;
+import com.caucho.quercus.env.StringBuilderValue;
+import com.caucho.quercus.env.StringValue;
+import com.caucho.quercus.env.StringValueImpl;
+import com.caucho.quercus.env.Value;
+
+import com.caucho.quercus.module.AbstractQuercusModule;
+import com.caucho.quercus.module.NotNull;
+import com.caucho.quercus.module.ReturnNullAsFalse;
+import com.caucho.quercus.module.Optional;
+
+import com.caucho.util.L10N;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Map;
+
+public class JsonModule
+    extends AbstractQuercusModule
+{
+  private static final Logger log
+    = Logger.getLogger(JsonModule.class.getName());
+  private static final L10N L = new L10N(JsonModule.class);
+
+  public String []getLoadedExtensions()
+  {
+    return new String[] { "json" };
+  }
+
+  /**
+   * Returns a JSON-encoded String.
+   *
+   * JSON strings can be in any Unicode format (UTF-8, UTF-16, UTF-32).
+   * Therefore need to pay special attention to multi-char characters.
+   *
+   * @param env
+   * @param val to encode into json format
+   * @return String JSON-encoded String
+   */
+  public StringValue json_encode(Env env, Value val)
+  {
+    StringBuilderValue sb = new StringBuilderValue();
+
+    jsonEncodeImpl(env, sb, val);
+    return sb;
+  }
+
+  private void jsonEncodeImpl(Env env, StringBuilderValue sb, Value val)
+  {
+    if (val instanceof StringValue) {
+      sb.append('"');
+      encodeString(sb, (StringValue)val);
+      sb.append('"');
+    }
+
+    else if (val == BooleanValue.TRUE)
+      sb.append("true");
+    else if (val == BooleanValue.FALSE)
+      sb.append("false");
+
+    else if (val instanceof NumberValue)
+      sb.append(val.toStringValue());
+
+    else if (val instanceof ArrayValue)
+      encodeArray(env, sb, (ArrayValue)val);
+
+    else if (val instanceof ObjectValue)
+      encodeObject(env, sb, (ObjectValue)val);
+
+    else if (val == NullValue.NULL || val == null)
+      sb.append("null");
+
+    else {
+      env.warning(L.l("type is unsupported; encoded as null"));
+    }
+  }
+
+  private void encodeArray(Env env, StringBuilderValue sb, ArrayValue val)
+  {
+    long length = 0;
+    for (Value key : val.keySet()) {
+      if ((! key.isLongConvertible()) || key.toLong() != length) {
+        encodeArrayToObject(env, sb, val);
+        return;
+      }
+      length++;
+    }
+
+    sb.append('[');
+
+    length = 0;
+    for (Value value : ((ArrayValue)val).values()) {
+      if (length > 0)
+        sb.append(',');
+      jsonEncodeImpl(env, sb, value);
+      length++;
+    }
+
+    sb.append(']');
+  }
+
+  /**
+   * Encodes an associative array into a JSON object.
+   */
+  private void encodeArrayToObject(Env env, StringBuilderValue sb, ArrayValue val)
+  {
+      sb.append('{');
+
+      int length = 0;
+      for (Map.Entry<Value,Value> entry : val.entrySet()) {
+        if (length > 0)
+          sb.append(',');
+
+        jsonEncodeImpl(env, sb, entry.getKey().toStringValue());
+        sb.append(':');
+        jsonEncodeImpl(env, sb, entry.getValue());
+        length++;
+      }
+
+      sb.append('}');
+  }
+
+  /**
+   * Encodes an PHP object into a JSON object.
+   */
+  private void encodeObject(Env env, StringBuilderValue sb, ObjectValue val)
+  {
+      sb.append('{');
+
+      int length = 0;
+      for (Map.Entry<String,Value> entry : val.entrySet()) {
+        if (length > 0)
+          sb.append(',');
+
+        jsonEncodeImpl(env, sb, new StringValueImpl(entry.getKey()));
+        sb.append(':');
+        jsonEncodeImpl(env, sb, entry.getValue());
+        length++;
+      }
+
+      sb.append('}');
+  }
+
+  /**
+   * Escapes special/control characters.
+   */
+  private void encodeString(StringBuilderValue sb, StringValue val)
+  {
+    int len = val.length();
+    for (int i = 0; i < len; i++) {
+      char c = val.charAt(i);
+
+      switch (c) {
+        case '\b':
+          sb.append('\\');
+          sb.append('b');
+          break;
+        case '\f':
+          sb.append('\\');
+          sb.append('f');
+          break;
+        case '\n':
+          sb.append('\\');
+          sb.append('n');
+          break;
+        case '\r':
+          sb.append('\\');
+          sb.append('r');
+          break;
+        case '\t':
+          sb.append('\\');
+          sb.append('t');
+          break;
+        case '\\':
+          sb.append('\\');
+          sb.append('\\');
+          break;
+        case '"':
+          sb.append('\\');
+          sb.append('"');
+          break;
+        case '/':
+          sb.append('\\');
+          sb.append('/');
+          break;
+        default:
+          if (c > 0x1F) {
+            sb.append(c);
+            break;
+          }
+
+          // Need to escape control chars in range 0-0x1F
+          sb.append('\\');
+          sb.append('u');
+          sb.append('0');
+          sb.append('0');
+
+          if (c <= 0x0F)
+            sb.append('0');
+          else {
+            sb.append('1');
+            c &= 0x0F;
+          }
+
+          if (c <= 0x09)
+            c += '0';
+          else
+            c += 'a' - 10;
+
+          sb.append(c);
+      }
+    }
+  }
+
+  /**
+   * Takes a JSON-encoded string and returns a PHP value.
+   *
+   * @param env
+   * @param s JSON-encoded string.
+   * @param assoc determines whether a generic PHP object or PHP associative
+   *     array should be returned when decoding json objects.
+   * @return decoded PHP value.
+   */
+  public Value json_decode(Env env,
+                          StringValue s,
+                          @Optional("false") boolean assoc)
+  {
+    return (new JsonDecoder()).jsonDecode(env, s, assoc);
+  }
+
+}
