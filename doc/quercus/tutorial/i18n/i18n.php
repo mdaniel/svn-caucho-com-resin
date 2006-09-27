10ce55257ffeae9a7d394afdfb825c762c90bb4a
nam <nam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2006-09-27 07:11:38 +0000

    i18 tutorial doc
    git-svn-id: svn://svn.caucho.com/resin/trunk@1630 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ doc/quercus/tutorial/i18n/i18n.php
@@ -0,0 +1,48 @@
+<html>
+<head>
+<title>Internationalization example</title>
+<body>
+
+<table border='1' cellpadding='5'>
+<tr>
+<th>English</th>
+<th>Translation</th>
+</tr>
+
+<tr>
+<td>Good morning</td>
+
+
+<td>
+<?php
+
+  setlocale(LC_MESSAGES, $_GET['language']);
+
+  bindtextdomain('messages', 'locale');
+
+  echo _("Good morning");
+
+?>
+</td>
+
+
+</tr>
+</table>
+
+<form>
+
+<p>
+Change locale to:
+<select name='language'>
+<option>none</option>
+<option value='de_DE'>de_DE (German)</option>
+<option value='en_AU'>en_AU (English)</option>
+<option value='fr_CA'>fr_CA (French)</option>
+
+</select>
+<input type='submit' value='Update'></input>
+</p>
+
+</form>
+</body>
+</html>
