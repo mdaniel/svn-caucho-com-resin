b68a8a40aed6c723fb6b963ec302398cacf7df0f
nam <nam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2006-10-10 12:20:17 +0000

    $_SERVER[HTTP_HOST] port inclusion
    git-svn-id: svn://svn.caucho.com/resin/trunk@1703 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/env/ServerArrayValue.java
+++ modules/quercus/src/com/caucho/quercus/env/ServerArrayValue.java
@@ -272,16 +272,10 @@ public class ServerArrayValue extends ArrayValueImpl {
       String value = request.getHeader(key);
 
       if (key.equalsIgnoreCase("Host")) {
-	// php/1b19 (yacs)
-	int p = value.indexOf(':');
-
-	if (p > 0)
-	  super.put(HTTP_HOST_V, new StringValueImpl(value.substring(0, p)));
-	else
-	  super.put(HTTP_HOST_V, new StringValueImpl(value));
+	    super.put(HTTP_HOST_V, new StringValueImpl(value));
       }
       else {
-	super.put(convertHttpKey(key), new StringValueImpl(value));
+        super.put(convertHttpKey(key), new StringValueImpl(value));
       }
     }
   }
