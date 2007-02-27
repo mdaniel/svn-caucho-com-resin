4664d63b5539dbef519e69e18c1ce287b260a050
westrupp <westrupp@9c94448d-38f1-0310-a231-d98308ff1ebf>
2007-02-27 04:49:21 +0000

    tck deployment issues
    git-svn-id: svn://svn.caucho.com/resin/trunk@2213 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/resin/src/com/caucho/server/deploy/ExpandDeployController.java
+++ modules/resin/src/com/caucho/server/deploy/ExpandDeployController.java
@@ -144,7 +144,7 @@ abstract public class ExpandDeployController<I extends DeployInstance>
    * Merges with the old controller.
    */
   @Override
-    protected void mergeController(DeployController oldControllerV)
+  protected void mergeController(DeployController oldControllerV)
   {
     super.mergeController(oldControllerV);
 
