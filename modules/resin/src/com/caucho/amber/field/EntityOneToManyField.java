7013272dfd88ad5b6fa686a41f8a1b7adc57537b
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2007-03-06 15:57:08 +0000

    build issues
    git-svn-id: svn://svn.caucho.com/resin/trunk@2244 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/resin/src/com/caucho/amber/field/EntityOneToManyField.java
+++ modules/resin/src/com/caucho/amber/field/EntityOneToManyField.java
@@ -213,7 +213,7 @@ public class EntityOneToManyField extends CollectionField {
     generateInternalCascade(out, aConn, cascadeType);
   }
 
-  private void generateInternalCascade(JavaWriter out,
+  protected void generateInternalCascade(JavaWriter out,
                                        String aConn,
                                        CascadeType cascadeType)
     throws IOException
