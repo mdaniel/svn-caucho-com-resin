b8d9648424e6d923a0e102fa7060b6a35111d985
sam <sam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2007-11-01 17:13:26 +0000

    eclipse plugin
    git-svn-id: svn://svn.caucho.com/resin/trunk@3325 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/eclipse-plugin/src/com/caucho/eclipse/ResinServerBehaviour.java
+++ modules/eclipse-plugin/src/com/caucho/eclipse/ResinServerBehaviour.java
@@ -1,4 +1,4 @@
-package org.eclipse.jst.server.generic.resin;
+package com.caucho.eclipse;
 
 import org.eclipse.jst.server.generic.core.internal.GenericServerBehaviour;
 
@@ -6,11 +6,11 @@ public class ResinServerBehaviour
   extends GenericServerBehaviour
 {
 
-  /* (non-Javadoc)
-* @see org.eclipse.wst.server.core.model.ServerBehaviourDelegate#stop(boolean)
-*/
   public void stop(boolean force)
   {
+    // change the default behaviour and always force the stop,
+    // which causes eclipse to just terminate the process and
+    // not run the <stop> defined in the serverdef file
     super.stop(true);
   }
 }
