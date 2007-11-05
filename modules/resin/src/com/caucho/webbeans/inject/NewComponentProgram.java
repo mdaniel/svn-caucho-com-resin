08db59014815083605c93a2a5d56b63253aad29c
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2007-11-05 03:25:27 +0000

    missing webbeans inject
    git-svn-id: svn://svn.caucho.com/resin/trunk@3343 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/resin/src/com/caucho/webbeans/inject/NewComponentProgram.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 1998-2006 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Scott Ferguson;
+ */
+
+package com.caucho.webbeans.inject;
+
+import com.caucho.config.*;
+import com.caucho.config.j2ee.*;
+
+import java.util.logging.*;
+
+public class NewComponentProgram extends BuilderProgram
+{
+  private static final Logger log
+    = Logger.getLogger(NewComponentProgram.class.getName());
+
+  private Class _type;
+  private AccessibleInject _inject;
+
+  public NewComponentProgram(Class type,
+			     AccessibleInject inject)
+  {
+    _type = type;
+    _inject = inject;
+  }
+
+  public void configureImpl(NodeBuilder builder, Object bean)
+    throws ConfigException
+  {
+    try {
+      Object value = _type.newInstance();
+
+      _inject.inject(bean, value);
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new ConfigException(e);
+    }
+  }
+
+  public Object configure(NodeBuilder builder, Class type)
+    throws ConfigException
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+}
