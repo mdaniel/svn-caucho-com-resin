6c527c076af4f12c42d6382fea9733d9fef782cf
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2007-11-16 03:07:05 +0000

    webbeans checkin
    git-svn-id: svn://svn.caucho.com/resin/trunk@3400 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/resin/src/com/caucho/webbeans/component/SingletonComponent.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 1998-2006 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Scott Ferguson
+ */
+
+package com.caucho.webbeans.component;
+
+import java.lang.annotation.*;
+import javax.webbeans.*;
+
+import com.caucho.webbeans.cfg.WbComponent;
+
+/**
+ * Component for a singleton beans
+ */
+public class SingletonComponent extends WbComponent {
+  private static final Object []NULL_ARGS = new Object[0];
+
+  private Object _value;
+
+  public SingletonComponent(Object value)
+  {
+    _value = value;
+  }
+
+  @Override
+  public void setScopeAnnotation(Annotation ann)
+  {
+  }
+
+  @Override
+  public Object getByName()
+  {
+    return _value;
+  }
+
+  @Override
+  public Object getInject()
+  {
+    return _value;
+  }
+
+  @Override
+  public Object get()
+  {
+    return _value;
+  }
+
+  protected Object createNew()
+  {
+    throw new IllegalStateException();
+  }
+}
