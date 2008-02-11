df43fea0432c0d4815a681af6cfa1ca448b25516
mo <mo@9c94448d-38f1-0310-a231-d98308ff1ebf>
2008-02-11 22:56:31 +0000

    require two back quote chars before USE substring is done
    git-svn-id: svn://svn.caucho.com/resin/trunk@3811 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/lib/db/Mysqli.java
+++ modules/quercus/src/com/caucho/quercus/lib/db/Mysqli.java
@@ -724,11 +724,13 @@ public class Mysqli extends JdbcConnectionResource {
         if (tok != null) {
           String dbname = tok.toString();
 
-	  // XXX: breaks mediawiki
-	  if (dbname.length() > 0 && dbname.charAt(0) == '`')
-	    dbname = dbname.substring(1);
-	  if (dbname.length() > 0 && dbname.charAt(dbname.length() - 1) == '`')
-	    dbname = dbname.substring(0, dbname.length() - 1);
+          // Extract database name if back quoted : "USE `DBNAME`"
+
+          if (dbname.length() >= 2 &&
+              dbname.charAt(0) == '`' &&
+              dbname.charAt(dbname.length() - 1) == '`') {
+            dbname = dbname.substring(1, dbname.length() - 1);
+          }
 
           setCatalog(dbname);
 
