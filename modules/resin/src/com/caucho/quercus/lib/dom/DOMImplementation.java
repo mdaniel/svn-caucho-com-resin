e210a8e83c3ea7c8377e626fc45f62d894d28cda
nam <nam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2008-10-01 19:08:15 +0000

    quercus html dom fix
    git-svn-id: svn://svn.caucho.com/resin/trunk@4698 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/resin/src/com/caucho/quercus/lib/dom/DOMImplementation.java
+++ modules/resin/src/com/caucho/quercus/lib/dom/DOMImplementation.java
@@ -377,7 +377,7 @@ public class DOMImplementation
   public void parseHTMLDocument(Document document, ReadStream is, String path)
     throws IOException, SAXException
   {
-    _factory.parseXMLDocument(document, is, path);
+    _factory.parseHTMLDocument(document, is, path);
   }
 
   public void parseXMLDocument(Document document, ReadStream is, String path)
