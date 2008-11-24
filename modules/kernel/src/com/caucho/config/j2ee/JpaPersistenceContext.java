e0b79532f2c18dba707ab17d4473ffc78ce36aee
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2008-11-24 03:14:21 +0000

    missing file
    git-svn-id: svn://svn.caucho.com/resin/trunk@4843 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/kernel/src/com/caucho/config/j2ee/JpaPersistenceContext.java
@@ -0,0 +1,28 @@
+package com.caucho.config.j2ee;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+import java.lang.annotation.*;
+
+import javax.webbeans.BindingType;
+
+/**
+ * JpaPersistenceContext is a binding type for the JPA EntityManager, letting
+ * you select the context by its name and allowing the extended value to
+ * be selected.
+ */
+@BindingType
+@Documented
+@Target({TYPE,FIELD,METHOD,PARAMETER})
+@Retention(RUNTIME)
+public @interface JpaPersistenceContext {
+  /**
+   * The unitName of the JPA Persistence Context
+   */
+  public String value();
+
+  /**
+   * If true, return the extended persistence context, defaults to false.
+   */
+  public boolean extended() default false;
+}
