5aa5ccdb4af0d538b2d837e14ac2872d5a29de7a
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2009-01-18 18:55:19 +0000

    hmtp missing files
    git-svn-id: svn://svn.caucho.com/resin/trunk@5076 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/resin/src/com/caucho/remote/HmtpServlet.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 1998-2008 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Scott Ferguson
+ */
+
+package com.caucho.remote;
+
+import java.io.*;
+import javax.servlet.*;
+
+import com.caucho.config.ConfigException;
+import com.caucho.hemp.*;
+import com.caucho.hemp.broker.*;
+import com.caucho.hemp.servlet.*;
+import com.caucho.bam.BamBroker;
+import com.caucho.server.connection.*;
+import com.caucho.util.L10N;
+import com.caucho.vfs.*;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Main protocol handler for the HTTP version of HeMPP.
+ */
+public class HmtpServlet extends GenericServlet {
+  private static final L10N L = new L10N(HmtpServlet.class);
+  
+  /**
+   * Service handling
+   */
+  public void service(ServletRequest request, ServletResponse response)
+    throws IOException, ServletException
+  {
+    HttpServletRequestImpl req = (HttpServletRequestImpl) request;
+    HttpServletResponseImpl res = (HttpServletResponseImpl) response;
+
+    String upgrade = req.getHeader("Upgrade");
+
+    if (! "HMTP/0.9".equals(upgrade)) {
+      // eventually can use alt method
+      res.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+
+    ReadStream is = req.getConnection().getReadStream();
+    WriteStream os = req.getConnection().getWriteStream();
+
+    BamBroker broker = HempBroker.getCurrent();
+
+    TcpDuplexController controller
+      = res.upgradeProtocol(new ServerFromLinkStream(broker, is, os));
+    
+    controller.setIdleTimeMax(30 * 60 * 1000L);
+  }
+}
