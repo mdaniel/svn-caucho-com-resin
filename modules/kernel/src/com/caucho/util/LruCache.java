6c932793c65ccf0fc0642476b09a097e82a9c2ec
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2009-02-25 20:10:00 +0000

    lrucache sync issue
    git-svn-id: svn://svn.caucho.com/resin/trunk@5256 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/kernel/src/com/caucho/util/LruCache.java
+++ modules/kernel/src/com/caucho/util/LruCache.java
@@ -474,8 +474,9 @@ public final class LruCache<K,V> {
 	return false;
     }
 
-    if (tail._value instanceof LruListener)
-      ((LruListener) tail._value).lruEvent();
+    V oldValue = tail._value;
+    if (oldValue instanceof LruListener)
+      ((LruListener) oldValue).lruEvent();
     
     V value = remove(tail._key);
     
