fdefe1dae93d2c64b3cd93201aae30352b12fe5e
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2009-06-10 18:00:57 +0000

    L10N enhancements for URLs
    git-svn-id: svn://svn.caucho.com/resin/trunk@5668 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/kernel/src/com/caucho/util/L10N.java
+++ modules/kernel/src/com/caucho/util/L10N.java
@@ -19,7 +19,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Resin Open Source; if not, write to the
- *   Free SoftwareFoundation, Inc.
+ *
+ *   Free Software Foundation, Inc.
  *   59 Temple Place, Suite 330
  *   Boston, MA 02111-1307  USA
  *
@@ -29,8 +30,10 @@
 package com.caucho.util;
 
 import java.io.InputStream;
+import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -40,34 +43,525 @@ import java.util.logging.Logger;
 public class L10N {
   private static Logger _log;
   
-  private static HashMap<String,HashMap<String,String>> l10nMap
-    = new HashMap<String,HashMap<String,String>>();
+  private static final HashMap<String,Properties> _l10nMap
+    = new HashMap<String,Properties>();
+
+  private Class _cl;
   
-  HashMap<String,String> messages;
+  private Boolean _isMessageFileAvailable;
+  private Boolean _isStringFileAvailable;
+  
+  private Properties _messages;
+  private Properties _strings;
   
   public L10N(Class cl)
   {
-    String name = cl.getName().replace('.', '/');
+    _cl = cl;
+  }
+  
+  public String l(String msg)
+  {
+    msg = getTranslated(msg);
+    
+    msg = getTranslated(msg);
+
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+  
+  public String l(String msg, long l)
+  {
+    return l(msg, String.valueOf(l));
+  }
+  
+  public String l(String msg, Object o)
+  {
+    msg = getTranslated(msg);
+
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+  
+  public String l(String msg, Object o1, Object o2)
+  {
+    msg = getTranslated(msg);
+    
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o1);
+          i += 3;
+        }
+        else if (ch == '1') {
+          cb.append(o2);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+
+  public String l(String msg, Object o1, int i2)
+  {
+    return l(msg, o1, String.valueOf(i2));
+  }
+
+  public String l(String msg, int i1, int i2)
+  {
+    return l(msg, String.valueOf(i1), String.valueOf(i2));
+  }
+  
+  public String l(String msg, Object o1, Object o2, Object o3)
+  {
+    msg = getTranslated(msg);
+    
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o1);
+          i += 3;
+        }
+        else if (ch == '1') {
+          cb.append(o2);
+          i += 3;
+        }
+        else if (ch == '2') {
+          cb.append(o3);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+  
+  public String l(String msg, Object o1, Object o2, Object o3, Object o4)
+  {
+    msg = getTranslated(msg);
+    
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o1);
+          i += 3;
+        }
+        else if (ch == '1') {
+          cb.append(o2);
+          i += 3;
+        }
+        else if (ch == '2') {
+          cb.append(o3);
+          i += 3;
+        }
+        else if (ch == '3') {
+          cb.append(o4);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+  
+  public String l(String msg, Object o1, Object o2,
+                  Object o3, Object o4, Object o5)
+  {
+    msg = getTranslated(msg);
+    
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o1);
+          i += 3;
+        }
+        else if (ch == '1') {
+          cb.append(o2);
+          i += 3;
+        }
+        else if (ch == '2') {
+          cb.append(o3);
+          i += 3;
+        }
+        else if (ch == '3') {
+          cb.append(o4);
+          i += 3;
+        }
+        else if (ch == '4') {
+          cb.append(o5);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+  
+  public String l(String msg, Object o1, Object o2,
+                  Object o3, Object o4, Object o5, Object o6)
+  {
+    msg = getTranslated(msg);
+    
+    StringBuilder cb = new StringBuilder();
+
+    int length = msg.length();
+    int i = 0;
+
+    while (i < length) {
+      char ch = msg.charAt(i);
+
+      if (ch != '{' || i + 2 >= length) {
+        cb.append(ch);
+        i++;
+      }
+      else {
+        ch = msg.charAt(i + 1);
+        
+        if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else if (ch == '0') {
+          cb.append(o1);
+          i += 3;
+        }
+        else if (ch == '1') {
+          cb.append(o2);
+          i += 3;
+        }
+        else if (ch == '2') {
+          cb.append(o3);
+          i += 3;
+        }
+        else if (ch == '3') {
+          cb.append(o4);
+          i += 3;
+        }
+        else if (ch == '4') {
+          cb.append(o5);
+          i += 3;
+        }
+        else if (ch == '5') {
+          cb.append(o6);
+          i += 3;
+        }
+        else if (ch == '{') {
+          cb.append('{');
+          i += 2;
+        }
+        else {
+	  i = parseString(msg, i + 1, cb);
+        }
+      }
+    }
+    
+    return cb.toString();
+  }
+
+  private int parseString(String msg, int i, StringBuilder sb)
+  {
+    StringBuilder arg = new StringBuilder();
+    int len = msg.length();
+    char ch;
+
+    for (; i < len && (ch = msg.charAt(i)) != '}'; i++) {
+      arg.append(ch);
+    }
+
+    Properties stringMap = getStringMap();
+
+    if (stringMap != null) {
+      String string = (String) stringMap.get(arg.toString());
+
+      if (string != null) {
+	sb.append(string);
+	return i + 1;
+      }
+    }
+
+    sb.append('{');
+    sb.append(arg);
+    
+    return i;
+  }
+
+  private String getTranslated(String msg)
+  {
+    Properties messages = getMessageMap();
+    
+    if (messages == null)
+      return msg;
+    
+    String translated = (String) messages.get(msg);
+
+    if (translated == null) {
+      return msg;
+    }
+    else
+      return translated;
+  }
+
+  private Properties getMessageMap()
+  {
+    if (_messages != null)
+      return _messages;
+    
+    if (Boolean.FALSE.equals(_isMessageFileAvailable))
+      return null;
+    
+    String name = _cl.getName().replace('.', '/');
     int p = name.lastIndexOf('/');
 
     if (p > 0)
-      init(name.substring(0, p) + "/messages");
-    else
-      init("/messages");
+      name = name.substring(0, p);
+
+    name = name + "/messages";
+
+    _messages = _l10nMap.get(name);
+
+    if (_messages != null) {
+      _isMessageFileAvailable = Boolean.TRUE;
+      
+      return _messages;
+    }
+
+    _messages = loadProperties(name);
+
+    if (_messages != null) {
+      _isMessageFileAvailable = Boolean.TRUE;
+      _l10nMap.put(name, _messages);
+      return _messages;
+    }
+    else {
+      _isMessageFileAvailable = Boolean.FALSE;
+      return null;
+    }
   }
-/*  
-  public L10N(String path)
+
+  private Properties getStringMap()
   {
-    init(path);
+    if (_strings != null)
+      return _strings;
+    
+    if (Boolean.FALSE.equals(_isStringFileAvailable))
+      return null;
+    
+    String name = _cl.getName().replace('.', '/');
+    int p = name.lastIndexOf('/');
+
+    if (p > 0)
+      name = name.substring(0, p);
+
+    name = name + "/strings";
+
+    _strings = _l10nMap.get(name);
+
+    if (_strings != null) {
+      _isStringFileAvailable = Boolean.TRUE;
+      
+      return _strings;
+    }
+
+    _strings = loadProperties(name);
+
+    if (_strings != null) {
+      _isStringFileAvailable = Boolean.TRUE;
+      _l10nMap.put(name, _strings);
+      return _strings;
+    }
+    else {
+      _isStringFileAvailable = Boolean.FALSE;
+      return null;
+    }
   }
-*/
+
+  private Properties loadProperties(String name)
+  {
+    InputStream is = null;
+    
+    try {
+      name = name + ".properties";
+      
+      is = _cl.getClassLoader().getResourceAsStream(name);
+
+      if (is != null) {
+	Properties messages = new Properties();
+	messages.load(is);
+
+	return messages;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      // can't log in logging routines
+    } finally {
+      IoUtil.close(is);
+    }
+
+    return null;
+  }
 
   private void init(String path)
   {
     if (! path.startsWith("/"))
       path = "/" + path;
 
-    messages = l10nMap.get(path);
+    Properties messages = _l10nMap.get(path);
     
     InputStream is = null;
     Locale locale = Locale.getDefault();
@@ -75,7 +569,7 @@ public class L10N {
     try {
       String language = locale.getLanguage();
 
-      String xmlName = path + "_" + language + ".xml";
+      String xmlName = path + "_" + language;
 
       /* XXX: punt for now
       is = getClass().getResourceAsStream(xmlName);
@@ -115,355 +609,11 @@ public class L10N {
       log().log(Level.FINE, e.toString(), e);
     }
   }
-  
-  public String l(String msg)
-  {
-    msg = getTranslated(msg);
-    
-    return msg;
-  }
-  
-  public String l(String msg, long l)
-  {
-    return l(msg, String.valueOf(l));
-  }
-  
-  public String l(String msg, Object o)
-  {
-    msg = getTranslated(msg);
-
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-  
-  public String l(String msg, Object o1, Object o2)
-  {
-    msg = getTranslated(msg);
-    
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o1);
-          i += 3;
-        }
-        else if (ch == '1') {
-          cb.append(o2);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-
-  public String l(String msg, Object o1, int i2)
-  {
-    return l(msg, o1, String.valueOf(i2));
-  }
-
-  public String l(String msg, int i1, int i2)
-  {
-    return l(msg, String.valueOf(i1), String.valueOf(i2));
-  }
-  
-  public String l(String msg, Object o1, Object o2, Object o3)
-  {
-    msg = getTranslated(msg);
-    
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o1);
-          i += 3;
-        }
-        else if (ch == '1') {
-          cb.append(o2);
-          i += 3;
-        }
-        else if (ch == '2') {
-          cb.append(o3);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-  
-  public String l(String msg, Object o1, Object o2, Object o3, Object o4)
-  {
-    msg = getTranslated(msg);
-    
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o1);
-          i += 3;
-        }
-        else if (ch == '1') {
-          cb.append(o2);
-          i += 3;
-        }
-        else if (ch == '2') {
-          cb.append(o3);
-          i += 3;
-        }
-        else if (ch == '3') {
-          cb.append(o4);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-  
-  public String l(String msg, Object o1, Object o2,
-                  Object o3, Object o4, Object o5)
-  {
-    msg = getTranslated(msg);
-    
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o1);
-          i += 3;
-        }
-        else if (ch == '1') {
-          cb.append(o2);
-          i += 3;
-        }
-        else if (ch == '2') {
-          cb.append(o3);
-          i += 3;
-        }
-        else if (ch == '3') {
-          cb.append(o4);
-          i += 3;
-        }
-        else if (ch == '4') {
-          cb.append(o5);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-  
-  public String l(String msg, Object o1, Object o2,
-                  Object o3, Object o4, Object o5, Object o6)
-  {
-    msg = getTranslated(msg);
-    
-    CharBuffer cb = CharBuffer.allocate();
-
-    int length = msg.length();
-    int i = 0;
-
-    while (i < length) {
-      char ch = msg.charAt(i);
-
-      if (ch != '{' || i + 2 >= length) {
-        cb.append(ch);
-        i++;
-      }
-      else {
-        ch = msg.charAt(i + 1);
-        
-        if (ch == '{') {
-          cb.append('{');
-          i += 2;
-        }
-        else if (msg.charAt(i + 2) != '}') {
-          cb.append('{');
-          i++;
-        }
-        else if (ch == '0') {
-          cb.append(o1);
-          i += 3;
-        }
-        else if (ch == '1') {
-          cb.append(o2);
-          i += 3;
-        }
-        else if (ch == '2') {
-          cb.append(o3);
-          i += 3;
-        }
-        else if (ch == '3') {
-          cb.append(o4);
-          i += 3;
-        }
-        else if (ch == '4') {
-          cb.append(o5);
-          i += 3;
-        }
-        else if (ch == '5') {
-          cb.append(o6);
-          i += 3;
-        }
-        else {
-          cb.append('{');
-          i++;
-        }
-      }
-    }
-    
-    return cb.close();
-  }
-
-  private String getTranslated(String msg)
-  {
-    if (messages == null)
-      return msg;
-    
-    String translated = (String) messages.get(msg);
-
-    if (translated == null) {
-      return msg;
-    }
-    else
-      return translated;
-  }
 
   private Logger log()
   {
     if (_log == null)
-      _log = Log.open(L10N.class);
+      _log = Logger.getLogger(L10N.class.getName());
 
     return _log;
   }
