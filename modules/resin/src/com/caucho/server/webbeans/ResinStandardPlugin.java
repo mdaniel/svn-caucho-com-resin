bea0e1d02c420ff499803babb6883f91743f0615
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2009-07-03 04:02:45 +0000

    missing commit
    git-svn-id: svn://svn.caucho.com/resin/trunk@5740 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/resin/src/com/caucho/server/webbeans/ResinStandardPlugin.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 1998-2008 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Scott Ferguson
+ */
+
+package com.caucho.server.webbeans;
+
+import com.caucho.config.Config;
+import com.caucho.config.ConfigException;
+import com.caucho.config.annotation.ServiceBinding;
+import com.caucho.config.ServiceStartup;
+import com.caucho.config.cfg.BeansConfig;
+import com.caucho.config.inject.InjectManager;
+import com.caucho.config.inject.ProcessBeanImpl;
+import com.caucho.ejb.manager.EjbContainer;
+import com.caucho.jms.JmsMessageListener;
+import com.caucho.vfs.Path;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import javax.ejb.*;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AfterBeanDiscovery;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import javax.enterprise.inject.spi.ProcessBean;
+
+/**
+ * Standard XML behavior for META-INF/beans.xml
+ */
+public class ResinStandardPlugin implements Extension
+{
+  private InjectManager _manager;
+  private ClassLoader _classLoader;
+
+  public ResinStandardPlugin(InjectManager manager)
+  {
+    _manager = manager;
+    
+    _classLoader = Thread.currentThread().getContextClassLoader();
+  }
+
+  public void processAnnotatedType(@Observes ProcessAnnotatedType event)
+  {
+    AnnotatedType annotatedType = event.getAnnotatedType();
+
+    if (annotatedType == null)
+      return;
+
+    if (annotatedType.isAnnotationPresent(Stateful.class)
+	|| annotatedType.isAnnotationPresent(Stateless.class)
+	|| annotatedType.isAnnotationPresent(MessageDriven.class)
+	|| annotatedType.isAnnotationPresent(JmsMessageListener.class)) {
+      EjbContainer ejbContainer = EjbContainer.create();
+      
+      ejbContainer.createBean(annotatedType);
+      event.veto();
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    return getClass().getSimpleName() + "[]";
+  }
+}
