952627fae339774d5ade93da5a3a10021af47aab
ferg <ferg@9c94448d-38f1-0310-a231-d98308ff1ebf>
2009-09-23 23:43:01 +0000

    missing checkin
    git-svn-id: svn://svn.caucho.com/resin/trunk@6096 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/kernel/src/com/caucho/config/inject/AbstractObserverMethod.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 1998-2008 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Scott Ferguson
+ */
+
+package com.caucho.config.inject;
+
+import com.caucho.config.annotation.ServiceType;
+import com.caucho.config.program.FieldComponentProgram;
+import com.caucho.config.*;
+import com.caucho.config.j2ee.*;
+import com.caucho.config.program.Arg;
+import com.caucho.config.program.ConfigProgram;
+import com.caucho.config.program.ContainerProgram;
+import com.caucho.config.scope.ScopeContext;
+import com.caucho.config.types.*;
+import com.caucho.naming.*;
+import com.caucho.util.*;
+import com.caucho.config.*;
+import com.caucho.config.bytecode.*;
+import com.caucho.config.cfg.*;
+import com.caucho.config.event.ObserverImpl;
+import com.caucho.config.inject.AnnotatedTypeImpl;
+import com.caucho.config.program.BeanArg;
+
+import java.lang.reflect.*;
+import java.lang.annotation.*;
+import java.util.*;
+import java.util.logging.*;
+import java.io.*;
+
+import javax.annotation.*;
+import javax.enterprise.context.Dependent;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.event.Observes;
+import javax.enterprise.event.Reception;
+import javax.enterprise.event.TransactionPhase;
+import javax.enterprise.inject.AnnotationLiteral;
+import javax.enterprise.inject.Default;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Nonbinding;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.Stereotype;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.AnnotatedConstructor;
+import javax.enterprise.inject.spi.AnnotatedMethod;
+import javax.enterprise.inject.spi.AnnotatedParameter;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.ObserverMethod;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.InjectionTarget;
+import javax.inject.Named;
+import javax.inject.Qualifier;
+
+/**
+ * Common bean introspection for Produces and ManagedBean.
+ */
+abstract public class AbstractObserverMethod<T>
+  implements ObserverMethod<T>
+{
+  public Class<?> getBeanClass()
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+
+  public Type getObservedType()
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+
+  public Set<Annotation> getObservedQualifiers()
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+
+  public Reception getReception()
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+
+  public TransactionPhase getTransactionPhase()
+  {
+    throw new UnsupportedOperationException(getClass().getName());
+  }
+
+  abstract public void notify(T event);
+}
