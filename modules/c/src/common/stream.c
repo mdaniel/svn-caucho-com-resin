ac0bc6377201136350952e754f23fed4e324f040
alex <alex@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-02-22 19:19:37 +0000

    back port stream.c load-distribution changes
    git-svn-id: svn://svn.caucho.com/resin/branches/3.1@6796 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/c/src/common/stream.c
+++ modules/c/src/common/stream.c
@@ -1288,7 +1288,9 @@ select_host(cluster_t *cluster, time_t now)
   int i;
   int best_srun;
   int best_cost = 0x7fffffff;
-  
+  cluster_srun_t *cluster_srun;
+  srun_t *srun;
+
   size = cluster->srun_size;  
   if (size < 1)
     return -1;
@@ -1314,12 +1316,24 @@ select_host(cluster_t *cluster, time_t now)
   }
   
   cluster->round_robin_index = round_robin;
+
+   /* if round-robin server is failing, choose one randomly */
+   for (i = 0; i < size; i++) {
+      cluster_srun = &cluster->srun_list[round_robin];
+      srun = cluster_srun->srun;
+
+      if (! srun->is_dead)
+        break;
+
+      round_robin = (rand() & 0x7fffffff) % size;
+   }
+
   best_srun = round_robin;
 
   for (i = 0; i < size; i++) {
     int index = (i + round_robin) % size;
-    cluster_srun_t *cluster_srun = &cluster->srun_list[index];
-    srun_t *srun = cluster_srun->srun;
+    cluster_srun = &cluster->srun_list[index];
+    srun = cluster_srun->srun;
     /* int tail; */
     int cost;
 
