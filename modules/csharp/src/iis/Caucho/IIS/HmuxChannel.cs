604bfba84f0ef946733d757211ade189f17d7cc4
alex <alex@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-03-25 01:33:07 +0000

    init iis
    git-svn-id: svn://svn.caucho.com/resin/trunk@6933 9c94448d-38f1-0310-a231-d98308ff1ebf

---
+++ modules/csharp/src/iis/Caucho/IIS/HmuxChannel.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Caucho.IIS
+{
+  class HmuxChannel
+  {
+    public static int HMUX_CHANNEL = 'C';
+    public static int HMUX_ACK = 'A';
+    public static int HMUX_ERROR = 'E';
+    public static int HMUX_YIELD = 'Y';
+    public static int HMUX_QUIT = 'Q';
+    public static int HMUX_EXIT = 'X';
+
+    public static int HMUX_DATA = 'D';
+    public static int HMUX_URI = 'U';
+    public static int HMUX_STRING = 'S';
+    public static int HMUX_HEADER = 'H';
+    public static int HMUX_BINARY = 'B';
+    public static int HMUX_PROTOCOL = 'P';
+    public static int HMUX_META_HEADER = 'M';
+
+    // The following are HTTP codes
+    public static int CSE_NULL = '?';
+    public static int CSE_PATH_INFO = 'b';
+    public static int CSE_PROTOCOL = 'c';
+    public static int CSE_REMOTE_USER = 'd';
+    public static int CSE_QUERY_STRING = 'e';
+    public static int HMUX_FLUSH = 'f';
+    public static int CSE_SERVER_PORT = 'g';
+    public static int CSE_REMOTE_HOST = 'h';
+    public static int CSE_REMOTE_ADDR = 'i';
+    public static int CSE_REMOTE_PORT = 'j';
+    public static int CSE_REAL_PATH = 'k';
+    public static int CSE_SCRIPT_FILENAME = 'l';
+    public static int HMUX_METHOD = 'm';
+    public static int CSE_AUTH_TYPE = 'n';
+    public static int CSE_URI = 'o';
+    public static int CSE_CONTENT_LENGTH = 'p';
+    public static int CSE_CONTENT_TYPE = 'q';
+    public static int CSE_IS_SECURE = 'r';
+    public static int HMUX_STATUS = 's';
+    public static int CSE_CLIENT_CERT = 't';
+    public static int CSE_SERVER_TYPE = 'u';
+    public static int HMUX_SERVER_NAME = 'v';
+
+    public static int CSE_SEND_HEADER = 'G';
+
+    private Socket _socket;
+    private TempBuffer _buffer;
+
+    public HmuxChannel(Socket socket)
+    {
+      _socket = socket;
+      _buffer = new TempBuffer(8888);
+    }
+
+    public int Read(byte[] buffer)
+    {
+      return _socket.Receive(buffer);
+    }
+
+    public void StartChannel()
+    {
+      byte[] bytes = new byte[] { (byte)HMUX_CHANNEL, 0, 1 };
+      _buffer.Write(bytes);
+    }
+
+    public void WriteUrl(String path)
+    {
+      _buffer.Write((byte)HMUX_URI);
+      byte[] bytes = System.Text.Encoding.ASCII.GetBytes(path.ToCharArray());
+      WriteRawString(bytes);
+    }
+
+    public void WriteRawString(byte[] bytes)
+    {
+      int len = bytes.Length;
+      _buffer.Write((byte)(len >> 8));
+      _buffer.Write((byte)len);
+      _buffer.Write(bytes);
+    }
+
+    public void WriteMethod(String method)
+    {
+      _buffer.Write((byte)HMUX_METHOD);
+      byte[] bytes = System.Text.Encoding.ASCII.GetBytes(method.ToCharArray());
+      WriteRawString(bytes);
+    }
+
+    public void WriteQuit()
+    {
+      _buffer.Write((byte)HMUX_QUIT);
+    }
+
+    public void WriteExit()
+    {
+      _buffer.Write((byte)HMUX_EXIT);
+    }
+
+    public void Flush()
+    {
+      _socket.Send(_buffer.Bytes, 0, _buffer.Lenght, SocketFlags.None);
+    }
+  }
+
+  /**
+   * pooled buffer
+   */
+  class TempBuffer
+  {
+    public int Lenght { get { return _length; } }
+    public byte[] Bytes { get { return _bytes; } }
+
+    private int _length;
+    private byte[] _bytes;
+
+    public TempBuffer(int capacity)
+    {
+      _bytes = new byte[capacity];
+      _length = 0;
+    }
+
+    public void Write(byte b)
+    {
+      if (_length == _bytes.Length) {
+        byte[] temp = _bytes;
+        _bytes = new byte[2 * temp.Length];
+        Array.Copy(temp, 0, _bytes, 0, _length);
+      }
+      _bytes[_length] = b;
+      _length++;
+    }
+
+    public void Write(byte[] buffer)
+    {
+      Write(buffer, 0, buffer.Length);
+    }
+
+    public void Write(byte[] buffer, int index, int length)
+    {
+      if (_length + length > _bytes.Length) {
+        byte[] temp = _bytes;
+        _bytes = new byte[_bytes.Length + length * 2];
+        Array.Copy(temp, 0, _bytes, 0, _length);
+      }
+
+      Array.Copy(buffer, index, _bytes, _length, length);
+      _length = _length + length;
+    }
+  }
+}
---
+++ modules/csharp/src/iis/Caucho/IIS/HmuxHandler.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Net.Sockets;
+
+namespace Caucho.IIS
+{
+  public class HmuxHandler : IHttpHandler
+  {
+    public bool IsReusable
+    {
+      get { return true; }
+    }
+
+    public void ProcessRequest(HttpContext context)
+    {
+      String path = context.Request.Path;
+      if ("/__caucho__test__basic".Equals(path)) {
+        DoTestBasic(context);
+      } else if ("/__caucho__test__chunked".Equals(path)) {
+        DoTestChunked(context);
+      } else {
+        DoHmux(context);
+      }
+    }
+
+    public void DoHmux(HttpContext context)
+    {
+      Socket hmuxSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+      hmuxSocket.Connect("localhost", 6810);
+      HmuxChannel channel = new HmuxChannel(hmuxSocket);
+      channel.StartChannel();
+      channel.WriteUrl(context.Request.RawUrl);
+      channel.WriteMethod(context.Request.HttpMethod);
+      channel.WriteQuit();
+      channel.WriteExit();
+      channel.Flush();
+
+      byte []buffer = new byte[1024];
+      int x = channel.Read(buffer);
+      context.Response.OutputStream.Write(buffer, 0, x);
+
+/**      hmuxSocket.Send(
+      cse_write_byte(hmuxSocket, HMUX_CHANNEL);
+      cse_write_byte(hmuxSocket, channel >> 8);
+      cse_write_byte(hmuxSocket, channel);
+
+      cse_write_string(hmuxSocket, HMUX_URL, uri);
+      write_var(hmuxSocket, r, "REQUEST_METHOD", HMUX_METHOD);
+      write_var(hmuxSocket, r, "SERVER_PROTOCOL", CSE_PROTOCOL);
+      //	write_var(hmuxSocket, r, "PATH_TRANSLATED", CSE_PATH_TRANSLATED);
+      write_var(hmuxSocket, r, "QUERY_STRING", CSE_QUERY_STRING);
+      write_var(hmuxSocket, r, "SERVER_NAME", HMUX_SERVER_NAME);
+      write_var(hmuxSocket, r, "SERVER_PORT", CSE_SERVER_PORT);
+      write_var(hmuxSocket, r, "REMOTE_HOST", CSE_REMOTE_HOST);
+      write_var(hmuxSocket, r, "REMOTE_ADDR", CSE_REMOTE_ADDR);
+      write_var(hmuxSocket, r, "REMOTE_USER", CSE_REMOTE_USER);
+      write_var(hmuxSocket, r, "AUTH_TYPE", CSE_AUTH_TYPE);
+
+      cse_write_string(hmuxSocket, CSE_SERVER_TYPE, "ISAPI");
+*/
+    }
+
+    public void DoTestBasic(HttpContext context)
+    {
+      context.Response.Write("Basic");
+    }
+
+    public void DoTestChunked(HttpContext context)
+    {
+      for (int i = 0; i < 10; i++) {
+        context.Response.Write("chunk:" + i);
+        context.Response.Flush();
+      }
+    }
+  }
+}
