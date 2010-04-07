1d7129532d94abe9cf3f30573d700d9da55b4b2c
emil <emil@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-04-07 00:35:21 +0000

    doc cleanup
    git-svn-id: svn://svn.caucho.com/resin/trunk@6979 9c94448d-38f1-0310-a231-d98308ff1ebf

--- doc/js/default.js
+++ doc/js/default.js
@@ -52,3 +52,140 @@ function toggleElementsByClass(checkbox, cssClass)
   }
 }
 
+function findElementLeft(element)
+{
+  var left = 0;
+
+  while (element != null) {
+    left += element.offsetLeft;
+    element = element.offsetParent;
+  }
+
+  return left;
+}
+
+function findElementTop(element)
+{
+  var top = 0;
+
+  while (element != null) {
+    top += element.offsetTop;
+    element = element.offsetParent;
+  }
+
+  return top;
+}
+
+/* class */ function Timer()
+{
+  var _ = this;
+
+  _.id = -1;
+
+  _.clear = function() {
+    if (_.id != -1) {
+      window.clearTimeout(_.id);
+      _.id = -1;
+    }
+  };
+
+  _.set = function(callback, timeout) {
+    _.clear();
+
+    _.id = window.setTimeout(callback, timeout);
+  };
+}
+
+/* class */ function Popup()
+{
+  var _ = this;
+
+  _.MAX_HEIGHT = 300;
+  _.MAX_WIDTH = 400;
+  _.xhr = new XMLHttpRequest();
+  _.element = document.getElementById('popup');
+  _.reference = null;
+  _.showTimer = new Timer();
+  _.hideTimer = new Timer();
+
+  _.element.onmouseover = function() {
+    _.hideTimer.clear();
+  };
+
+  _.element.onmouseout = function() {
+    _.mouseOutHandler();
+  };
+
+  _.show = function(caller, reference, text) {
+
+    _.showTimer.clear();
+
+    var top = findElementTop(caller);
+    var left = findElementLeft(caller);
+    var windowWidth = document.documentElement.clientWidth;
+
+    _.element.style.top = top + caller.offsetHeight + "px";
+
+    if (left + _.MAX_WIDTH > windowWidth - 30) {
+      _.element.style.left = windowWidth - _.MAX_WIDTH - 30 + "px";
+    }
+    else {
+      _.element.style.left = left + "px";
+    }
+
+    _.element.style.width = _.MAX_WIDTH + "px";
+    _.element.style.height = _.MAX_HEIGHT + "px";
+
+    _.element.innerHTML = text;
+    _.element.style.display = 'block';
+
+    _.reference = reference;
+  };
+
+  _.refResponseHandler = function(caller, reference) {
+    // 4 == DONE
+    if (_.xhr.readyState == 4 && _.xhr.status == 200) {
+      _.show(caller, reference, _.xhr.responseText);
+    }
+  };
+
+  _.fetchReference = function(caller, reference) {
+    _.xhr.onreadystatechange = function() {
+      _.refResponseHandler(caller, reference); 
+    };
+
+    _.xhr.open("GET", "reference/?ref=" + reference, true);
+    _.xhr.send();
+  };
+
+  _.mouseOverHandler = function(tag, reference) {
+    _.hideTimer.clear();
+
+    if (_.reference != reference) {
+      _.hide();
+    }
+
+    if (_.reference == null) {
+      var showTimeoutHandler = function() {
+        _.fetchReference(tag, reference);
+      };
+
+      _.showTimer.set(showTimeoutHandler, 1000);
+    }
+  };
+
+  _.hide = function() {
+    _.element.scrollTop = 0;
+    _.element.style.display = 'none';
+    _.element.innerHTML = '';
+
+    _.reference = null;
+  };
+
+  _.mouseOutHandler = function() {
+    _.showTimer.clear();
+
+    _.hideTimer.set(_.hide, 1000);
+  };
+}
+
