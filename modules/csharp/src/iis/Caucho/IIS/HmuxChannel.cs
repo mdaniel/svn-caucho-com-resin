a1b743aea771e3d975a0a6db824f991b4c9b92fc
alex <alex@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-04-07 18:52:11 +0000

    iis: copyright & comments
    git-svn-id: svn://svn.caucho.com/resin/trunk@6984 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/csharp/src/iis/Caucho/IIS/HmuxChannel.cs
+++ modules/csharp/src/iis/Caucho/IIS/HmuxChannel.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+ * Copyright (c) 1998-2010 Caucho Technology -- all rights reserved
+ *
+ * This file is part of Resin(R) Open Source
+ *
+ * Each copy or derived work must preserve the copyright notice and this
+ * notice unmodified.
+ *
+ * Resin Open Source is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Resin Open Source is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE, or any warranty
+ * of NON-INFRINGEMENT.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resin Open Source; if not, write to the
+ *
+ *   Free Software Foundation, Inc.
+ *   59 Temple Place, Suite 330
+ *   Boston, MA 02111-1307  USA
+ *
+ * @author Alex Rojkov
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -160,6 +189,7 @@ namespace Caucho.IIS
 
         RelayHeader("HTTPS", "on");
         RelayHeader("SSL_SECRETKEYSIZE", clientCertificate.KeySize.ToString());
+        //HTTPS_SECRETKEYSIZE XXX isn't used in HmuxRequest. Will be deleted by release time if no one objects.
         //RelayHeader("HTTPS_SECRETKEYSIZE", clientCertificate.SecretKeySize.ToString());
       }
 
@@ -169,11 +199,14 @@ namespace Caucho.IIS
         WriteHmuxLength(clientCertificate.Certificate.Length);
         Write(clientCertificate.Certificate);
 
-        RelayHeader("CERT_ISSUER", clientCertificate.Issuer);
-        RelayHeader("CERT_SERIALNUMBER", clientCertificate.SerialNumber);
-        RelayHeader("CERT_SUBJECT", clientCertificate.Subject);
-        RelayHeader("CERT_SERVER_ISSUER", clientCertificate.ServerIssuer);
-        RelayHeader("CERT_SERVER_SUBJECT", clientCertificate.ServerSubject);
+        /**
+         * XXX These aren't used by HmuxRequest. Will be deleted by release time if no one objects.
+         * RelayHeader("CERT_ISSUER", clientCertificate.Issuer);
+         * RelayHeader("CERT_SERIALNUMBER", clientCertificate.SerialNumber);
+         * RelayHeader("CERT_SUBJECT", clientCertificate.Subject);
+         * RelayHeader("CERT_SERVER_ISSUER", clientCertificate.ServerIssuer);
+         * RelayHeader("CERT_SERVER_SUBJECT", clientCertificate.ServerSubject);
+         */ 
       }
     }
 
