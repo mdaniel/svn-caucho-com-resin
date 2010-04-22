eaab7d543b7489faadd556d2070ce1bc938c6185
emil <emil@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-04-22 23:54:27 +0000

    restored section>description
    git-svn-id: svn://svn.caucho.com/resin/trunk@7040 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/webutil/src/com/caucho/xtpdoc/Section.java
+++ modules/webutil/src/com/caucho/xtpdoc/Section.java
@@ -152,12 +152,11 @@ public abstract class Section extends ContainerNode {
     return list;
   }
 
-  /*
   public FormattedTextWithAnchors createDescription()
   {
     _description = new FormattedTextWithAnchors(getDocument());
     return _description;
-  }*/
+  }
 
   public BlockQuote createBlockquote()
   {
