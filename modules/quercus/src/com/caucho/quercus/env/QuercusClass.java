0ca41d8ab722a9b722790d0680d7524bc13b2895
dorn <dorn@9c94448d-38f1-0310-a231-d98308ff1ebf>
2010-10-20 18:55:09 +0000

    fixing visibility issues with __get/__set/__unset/__isset
    git-svn-id: svn://svn.caucho.com/resin/trunk@7631 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/env/QuercusClass.java
+++ modules/quercus/src/com/caucho/quercus/env/QuercusClass.java
@@ -1079,12 +1079,17 @@ public class QuercusClass extends NullValue {
 
   /**
    * Implements the __get method call.
+   * __get() is utilized for reading data from inaccessible properties.
    */
   public Value getField(Env env, Value qThis, StringValue name)
   {
     // php/09km, php/09kn
     // push/pop to prevent infinite recursion
-    
+    if(issetField(name) && _fieldMap.get(name).isPublic())
+    {
+        return this.get(name); // TODO: move to ObjectExtValue if possible
+    }
+
     if (_fieldGet != null) {
       if (! env.pushFieldGet(Env.OVERLOADING_TYPES.FIELDGET, qThis.getClassName(), name))
         return UnsetValue.UNSET;
@@ -1099,8 +1104,15 @@ public class QuercusClass extends NullValue {
       return UnsetValue.UNSET;
   }
 
+  /**
+   * Implements the __isset method call.
+   * __isset() is triggered by calling isset() or empty()  on inaccessible properties.
+  */
   public Value issetField(Env env, Value qThis, StringValue name)
   {
+    if(issetField(name) && _fieldMap.get(name).isPublic())
+      return BooleanValue.TRUE;    // TODO: move to ObjectExtValue if possible
+
     // basically a copy of the __get code with slightly different semantics
     if (_isset != null) {
       if (! env.pushFieldGet(Env.OVERLOADING_TYPES.ISSET, qThis.getClassName(), name))
@@ -1131,10 +1143,19 @@ public class QuercusClass extends NullValue {
       _fieldMap.remove(name);
   }
 
+  /**
+   * implements the __unset method call
+   *  __unset() is invoked when unset() is used on inaccessible properties.
+   */
   public Value unsetField(Env env, Value qThis, StringValue name)
   {
-    // basically a copy of the __get code with slightly different semantics
+    if(issetField(name) && _fieldMap.get(name).isPublic()){
+      // TODO: move to ObjectExtValue if possible
+      unsetField(name);
+      return NullValue.NULL;
+    }
 
+    // basically a copy of the __get code with slightly different semantics
     if (_unset != null) {
       if (! env.pushFieldGet(Env.OVERLOADING_TYPES.UNSET, qThis.getClassName(), name))
         return UnsetValue.UNSET;
