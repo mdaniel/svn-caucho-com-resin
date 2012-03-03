23d9802b094ba1a91fa406ac99d35a44331a692a
rick <rick@9c94448d-38f1-0310-a231-d98308ff1ebf>
2012-03-03 19:47:32 +0000

    made JSON encoding ASCII to unicode safe /u007E to //u0080 to be exact, decoder does not care if it is utf 8 or utf 16 or utf whatever, but encoding assumes ASCII as per discussion here http://stackoverflow.com/questions/583562/json-character-encoding. This is inline with other JSON libraries
    git-svn-id: svn://svn.caucho.com/resin/trunk@8953 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/jamp_prototype_early_/base/core/src/com/caucho/encoder/JSONEncoder.java
+++ modules/jamp_prototype_early_/base/core/src/com/caucho/encoder/JSONEncoder.java
@@ -2,28 +2,80 @@ package com.caucho.encoder;
 
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-/** Converts a Java object into a JSON string. */
+/** Converts a Java object into a JSON string. 
+ * I opted for ASCII safe encodig after reading this: http://stackoverflow.com/questions/583562/json-character-encoding
+ * */
 public class JSONEncoder implements Encoder {
+    
+    /* Not sure of the speed of this. */
+    //private final static CharsetEncoder asciiEncoder = 
+    //    Charset.forName("US-ASCII").newEncoder(); // or "ISO-8859-1" for ISO Latin 1
+
+    
 	private String encodeString(String str) {
-		StringBuilder builder = new StringBuilder();
+		StringBuilder builder = new StringBuilder(str.length() + (int)(str.length() * 0.2f));
 		builder.append("\"");
 		char[] charArray = str.toCharArray();
+		
+		
 		for (int index=0; index < charArray.length; index++) {
 			char c = charArray[index];
+			
+			//see http://www.json.org/ to understand this case statement better under string train tracks
 			switch (c) {
-			case '\n':
-				builder.append('\\').append('n');
+            case '\"':
+                builder.append('\\').append('\"');
+                break;                
+            case '\\':
+                builder.append('\\').append('\\');
+                break;
+            case '/':
+                builder.append('\\').append('/');
+                break;               
+            case '\b':
+                builder.append('\\').append('b');
+                break;
+            case '\f':
+				builder.append('\\').append('f');
 			    break;
-			case '\"':
-				builder.append('\\').append('\"');
-			    break;
+            case '\n':
+                builder.append('\\').append('n');
+                break;
+            case '\r':
+                builder.append('\\').append('r');
+                break;
+            case '\t':
+                builder.append('\\').append('t');
+                break;
 
 			default:
-				builder.append(c);
+		         /* Encode unicode character. */
+	            //if (!asciiEncoder.canEncode(c)){ //This works to but worried it might be too slow http://en.wikipedia.org/wiki/ASCII
+			    if (c>0x7F) { //See if it is out of range of ASCII
+	                //I don't like this for performance, I am going to roll my own.
+	               //builder.append(String.format("\\u%4H", c).replace(' ', '0'));	                
+	                String hexString = Integer.toHexString(c).toUpperCase();
+	                builder.append('\\').append('u');
+
+	                if (hexString.length() >= 4) {
+	                    builder.append(hexString);
+	                } else {
+	                    int howMany0 = 4 - hexString.length();
+	                    for (int i = 0; i < howMany0; i++) {
+	                       builder.append('0'); 
+	                    }
+	                    builder.append(hexString);
+	                }
+	            } else {
+	                builder.append(c);
+	            }
+
 			}
 		}
 		builder.append("\"");
