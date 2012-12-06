d84158029b22e3651f31eca51670aef03fb0537e
nam <nam@9c94448d-38f1-0310-a231-d98308ff1ebf>
2012-12-06 15:47:08 +0000

    quercus refactor google vfs support2
    git-svn-id: svn://svn.caucho.com/home/svn/svnroot/resin/trunk@9841 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/quercus/src/com/caucho/quercus/GoogleQuercus.java
+++ modules/quercus/src/com/caucho/quercus/GoogleQuercus.java
@@ -31,7 +31,11 @@ package com.caucho.quercus;
 
 import com.caucho.quercus.env.Env;
 import com.caucho.quercus.env.GoogleEnv;
+import com.caucho.quercus.lib.db.JdbcDriverContext;
 import com.caucho.quercus.page.QuercusPage;
+import com.caucho.vfs.GoogleMergePath;
+import com.caucho.vfs.MergePath;
+import com.caucho.vfs.Path;
 import com.caucho.vfs.WriteStream;
 
 import javax.servlet.http.HttpServletRequest;
@@ -47,6 +51,48 @@ public class GoogleQuercus extends QuercusContext
    */
   public GoogleQuercus()
   {
+  }
+
+  @Override
+  public void init()
+  {
+    String mode
+      = System.getProperty("com.google.appengine.tools.development.ApplicationPreparationMode");
+
+    boolean isGsDisabled = "true".equals(mode);
+
+    if (! isGsDisabled) {
+      String gsBucket = getIniString("google.cloud_storage_bucket");
+
+      if (gsBucket != null) {
+        Path stdPwd = getPwd();
+
+        GoogleMergePath mergePwd = new GoogleMergePath(stdPwd, gsBucket, true);
+        setPwd(mergePwd);
+
+        Path webInfDir = getWebInfDir();
+        Path gsWebInfDir = mergePwd.getGooglePath().lookup("WEB-INF");
+        MergePath mergeWebInf = new MergePath(gsWebInfDir, webInfDir);
+
+        setWebInfDir(mergeWebInf);
+      }
+    }
+
+    super.init();
+
+    JdbcDriverContext jdbcDriverContext = getJdbcDriverContext();
+
+    String driver = getIniString("google.jdbc_driver");
+
+    if (driver == null) {
+      driver = "com.google.appengine.api.rdbms.AppEngineDriver";
+    }
+
+    jdbcDriverContext.setDefaultDriver(driver);
+    jdbcDriverContext.setDefaultUrlPrefix("jdbc:google:rdbms://");
+    jdbcDriverContext.setDefaultEncoding(null);
+    jdbcDriverContext.setProtocol("mysql", driver);
+    jdbcDriverContext.setProtocol("google:rdbms", driver);
   }
 
   @Override
