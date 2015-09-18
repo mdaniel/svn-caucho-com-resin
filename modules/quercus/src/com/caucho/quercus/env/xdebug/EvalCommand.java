3b7db8d68f3a2aad9da3a9bcec93b85c52dffbda
Immanuel Scheerer <immanuel.scheerer@apparentmedia.de>
2015-09-18 18:09:21 +0200

    Improved xdebug command "eval" to be able to handle objects and arrays.

--- modules/quercus/src/com/caucho/quercus/env/xdebug/EvalCommand.java
+++ modules/quercus/src/com/caucho/quercus/env/xdebug/EvalCommand.java
@@ -12,26 +12,11 @@ public class EvalCommand extends XdebugCommand
 		System.out.println("Requested eval expression: " + data);
 		Object value = conn.eval(data);
 
-		String type = null;
-		String size = null;
-		String serializedValue = "";
-		if (value instanceof Boolean) {
-			type = "bool";
-			serializedValue = ((Boolean) value) ? "1" : "0";
-		} else if (value instanceof String) {
-			type = "string";
-			size = "" + ((String) value).length();
-			serializedValue = javax.xml.bind.DatatypeConverter.printBase64Binary(((String) value).getBytes());
-		} else if (value == null) {
-			type ="null";
-			serializedValue = "";
-		} else {
-			System.err.println("unknown type: " + value.getClass());
-		}
-		String response = "<response xmlns=\"urn:debugger_protocol_v1\" xmlns:xdebug=\"http://xdebug.org/dbgp/xdebug\" command=\"eval\" transaction_id=\""
-				+ transactionId + "\"><property address=\"140734717470128\" type=\"" + type + "\"" + (size == null ? "" : " size=\"" + size + "\" encoding=\"base64\"") + ">"
-				+ "<![CDATA[" + serializedValue + "]]></property></response>";
-		
-	  return new XdebugResponse(null, response, transactionId);
+		StringBuilder sb = new StringBuilder();
+		sb.append("<response xmlns=\"urn:debugger_protocol_v1\" xmlns:xdebug=\"http://xdebug.org/dbgp/xdebug\" command=\"eval\" transaction_id=\""
+				+ transactionId + "\">");
+		sb.append(createPropertyElement(value, conn, null, null, null, true));
+		sb.append("</response>");
+	  return new XdebugResponse(null, sb.toString(), transactionId);
   }
 }
--- modules/quercus/src/com/caucho/quercus/env/xdebug/XdebugCommand.java
+++ modules/quercus/src/com/caucho/quercus/env/xdebug/XdebugCommand.java
@@ -1,12 +1,27 @@
 package com.caucho.quercus.env.xdebug;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
+
+import com.caucho.quercus.env.BooleanValue;
+import com.caucho.quercus.env.DoubleValue;
+import com.caucho.quercus.env.LongValue;
+import com.caucho.quercus.env.NullValue;
+import com.caucho.quercus.env.QuercusClass;
+import com.caucho.quercus.env.StringValue;
+import com.caucho.quercus.env.UnsetValue;
+import com.caucho.quercus.env.Value;
+import com.caucho.quercus.program.ClassField;
 
 public abstract class XdebugCommand
 {
 	public final static String PARAM_TRANSACTION_ID = "-i";
 	public final static String PARAM_DATA = "--";
+	public final static String CONTEXT_ID_LOCALS = "0";
+    public final static String CONTEXT_ID_GLOBALS = "1";
 	
 	public XdebugResponse getResponse(XdebugConnection conn, String[] requestParts) {
 		if (requestParts.length  < 1) {
@@ -41,4 +56,102 @@ public abstract class XdebugCommand
 	}
 	
 	protected abstract XdebugResponse getInternalResponse(String commandName, Map<String, String> parameters, String transactionId, XdebugConnection conn);
+
+  protected String createPropertyElement(Object value, XdebugConnection conn,
+      String name, String fullname, String facet, boolean includeChildren) {
+        String type = null;
+        String size = null;
+        String serializedValue = null;
+        String stringValue = null;
+        if (value instanceof Boolean || value instanceof BooleanValue) {
+          if (value instanceof BooleanValue) {
+            value = ((BooleanValue) value).toBoolean();
+          }
+          type = "bool";
+          serializedValue = ((Boolean) value) ? "1" : "0";
+        } else if (value instanceof String || value instanceof StringValue) {
+          type = "string";
+          stringValue = value.toString();
+        } else if (value instanceof Integer || value instanceof Long
+            || value instanceof BigDecimal || value instanceof LongValue) {
+          type = "int";
+          stringValue = value.toString();
+        } else if (value instanceof Double || value instanceof Float
+            || value instanceof DoubleValue) {
+          type = "float";
+          stringValue = value.toString();
+        } else if (value == null || value instanceof NullValue) {
+          type = "null";
+          serializedValue = "";
+        } else if (value instanceof UnsetValue) {
+          type = "undefined";
+          serializedValue = "";
+        } else if (!(value instanceof Value)) {
+          stringValue = "unknown type: " + value.getClass();
+          System.err.println(stringValue);
+          type = "string";
+        }
+        if (stringValue != null) {
+          size = "" + stringValue.length();
+          serializedValue = javax.xml.bind.DatatypeConverter
+              .printBase64Binary(stringValue.getBytes());
+        }
+        String propertyPrefix = "<property " + (name != null ? "name=\"" + name + "\" " : "")
+            + (fullname != null ? "fullname=\"" + fullname + "\" " : "") 
+            + (facet != null ? "facet=\"" + facet + "\" " : "");
+        if (serializedValue != null) {
+          return propertyPrefix + "type=\"" + type + "\""
+            + (size == null ? "" : " size=\"" + size + "\" encoding=\"base64\"")
+            + ">" + "<![CDATA[" + serializedValue + "]]></property>";
+        } else if (value instanceof Value) {
+          Value quercusValue = (Value) value;
+          QuercusClass quercusClass = quercusValue.getQuercusClass();
+          if (quercusClass != null && !quercusClass.isArray()) {
+            HashMap<StringValue, ClassField> fields = quercusClass.getClassFields();
+            StringBuilder sb = new StringBuilder();
+            sb.append(propertyPrefix + "type=\"object\" classname=\"" + quercusClass.getClassName() + "\" children=\"" + (fields.isEmpty() ? "0" : "1") 
+                + "\" numchildren=\"" + fields.size() + "\"" + (name == null ? " page=\"0\" pagesize=\"100\"" : "") + ">");
+            if (includeChildren) {
+              for (Entry<StringValue, ClassField> entry : fields.entrySet()) {
+                String propName = entry.getKey().toString();
+                sb.append(createPropertyElement(quercusValue.getField(conn.getEnv(), entry.getKey()), conn, propName, fullname == null ? null : fullname + "->" + propName, getFacet(entry.getValue()), false));
+              }
+            }
+            sb.append("</property>");
+            return sb.toString();
+          } else if (quercusValue.isArray()) {
+            StringBuilder sb = new StringBuilder();
+            int count = quercusValue.getCount(conn.getEnv());
+            sb.append(propertyPrefix + "type=\"array\" children=\"" + (count < 1 ? "0" : "1") 
+                + "\" numchildren=\"" + count + "\"" + (name == null ? " page=\"0\" pagesize=\"100\"" : "") + ">");
+            if (includeChildren) {
+              Iterator<Entry<Value, Value>> iter = quercusValue.getBaseIterator(conn.getEnv());
+              while (iter.hasNext()) {
+                Entry<Value, Value> entry = iter.next();
+                String indexName = entry.getKey().toString();
+                sb.append(createPropertyElement(entry.getValue(), conn, indexName, fullname == null ? null : fullname + "['" + indexName.replaceAll("'", "\\'") + "']", null, false));
+              }
+            }
+            sb.append("</property>");
+            return sb.toString();
+          } else {
+            throw new IllegalStateException("Could not handle value " + value);
+          }
+        } else {
+          throw new IllegalStateException("could not handle " + value);
+        }
+      }
+
+  private String getFacet(ClassField classField) {
+    if (classField.isPrivate()) {
+      return "private";
+    }
+    if (classField.isProtected()) {
+      return "protected";
+    }
+    if (classField.isPublic()) {
+      return "public";
+    }
+    return null;
+  }
 }
