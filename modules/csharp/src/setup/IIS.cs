bde43bf6af2177dcac2ef440dcd6286c4d3dc80e
alex <alex@9c94448d-38f1-0310-a231-d98308ff1ebf>
2016-03-31 17:13:14 +0000

    copyright updates.
    git-svn-id: svn://svn.caucho.com/home/svn/svnroot/resin/trunk@10405 9c94448d-38f1-0310-a231-d98308ff1ebf

--- modules/csharp/src/setup/IIS.cs
+++ modules/csharp/src/setup/IIS.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 1998-2015 Caucho Technology -- all rights reserved
+ * Copyright (c) 1998-2016 Caucho Technology -- all rights reserved
  *
  * This file is part of Resin(R) Open Source
  *
